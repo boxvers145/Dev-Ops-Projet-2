7cf7b5c7c4451d1a7c6a5f67fcb07873f588ddf4
boxvers145 <62158594+boxvers145@users.noreply.github.com>
2023-12-14 11:05:29 +0100

    create webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js', // point d'entrée de votre application
+  output: {
+    filename: 'main.js', // nom du fichier de sortie
+    path: path.resolve(__dirname, 'dist') // dossier de sortie
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // regex pour cibler les fichiers .js
+        exclude: /node_modules/, // exclure le dossier node_modules
+        use: {
+          loader: 'babel-loader' // utiliser babel-loader pour transpiler le code ES6+ en ES5
+        }
+      }
+    ]
+  }
+}
